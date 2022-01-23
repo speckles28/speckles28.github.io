eead8f79d828d293c4c17f37ac1e5f5826bf7771
sabertoaster <98017055+speckles28@users.noreply.github.com>
2022-01-23 21:41:19 +0700

    Template Complete

--- javascript/jquery.js
+++ javascript/jquery.js
@@ -1,3 +1,19 @@
+// tooltip initalize 
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+})
+// tooltip initalize 
+
+//toggle scroll
+window.addEventListener('scroll', (e) => {
+  const navDark = $('.navigation_bar_dropdown')
+  const napba = $('.nav-dropdown--container')
+  const navLight = $('.navigation_bar')
+  if (window.pageYOffset >= 75) {
+    napba.css('top', 0)
+    
+  } else {
+    napba.css('top', '-75px')
+  }
+})
+//toggle scroll
